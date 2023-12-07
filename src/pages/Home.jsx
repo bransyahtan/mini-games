3c473bdef060c08d105d994f19618d4bc25d28d9
bransyahtan <bransyahhhh@gmail.com>
2023-12-07 16:08:04 +0700

    first mini games

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-slate-800 flex justify-center">
+      <div className="container my-10 border border-white rounded-2xl">
+        <p className="text-white">aaaaaaaaaa</p>
+      </div>
+    </div>
+  );
+};
